171aaefbc97ce82945e681ce23bd2d0d68088263
sardaceylan <arda_ceylanrnwr@yahoo.com>
2022-04-06 00:02:47 +0300

    single commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoApp from "./components/TodoApp";
+
+
+// React Hook
+// Stateless Function Component + Hook => Class Component (state)
+
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
---
+++ src/components/ToDoList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TodoItem from "./ToDoItem";
+
+const TodoList = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.items.map((item, index) => (
+          <TodoItem deleteItem={props.deleteItem} key={index} item={item} />
+        ))}
+      </ul>
+      <p>
+        <button onClick={props.clearItems}>Clear Items</button>
+      </p>
+    </div>
+  );
+};
+
+export default TodoList;
